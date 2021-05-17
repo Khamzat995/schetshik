6868484e934a403cac70353565ccccd373a9e26b
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-05-17 14:26:11 +0300

    tamagosh

--- tamagochi.js
+++ tamagochi.js
@@ -1,18 +1,50 @@
 function createTamagochi() {
   const tamagochiObject = {
     name: 'Tamagochi',
-    food: 4,
+    food: 1,
     energy: 1,
-    mood: 3,
+    mood: 1,
 
     getStatus: function () {
+      if (this.food < 3) {
+         console.log(`Я голоден(${this.food})`);
+      } else {
+         console.log(`Я не голоден(${this.food})`) ;
+      }
+       if (this.energy < 3) {
+        console.log(`Я хочу спасть(${this.energy})`);
+      } else {
+         console.log(`Я не хочу спать(${this.energy})`);
+      }
+      if (this.mood < 3) {
+        console.log(`Мне скучно(${this.mood})`);
+      } else {
+        console.log(`Мне весело(${this.mood})`);
+      }
       return tamagochiObject;
     },
 
-    setName: function (chengeName) {
-      this.name = chengeName;
+    setName: function (name) {
+      this.name = name;
     },
 
+    eat: function () {
+      if (this.food < 5 || this.mood < 5) {
+        this.food++ && this.mood--;
+      }
+      },
+
+    sleep: function () {
+      if (this.energy < 5 || this.food < 5) {
+        this.energy++ && this.food--;
+      }
+    },
+
+    play: function () {
+      if (this.mood < 5 || this.energy < 5) {
+        this.mood++ && this.energy--;
+      }
+    }
   }
 
 
@@ -20,3 +52,6 @@ function createTamagochi() {
 }
 
 const tamagochi = createTamagochi();
+
+
+//eat sleep play
